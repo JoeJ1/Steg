0dc9b5f0f7bf9bf71909bdc1e0523761df8fb549
JoeJ1 <joeedwardharry1@gmail.com>
2021-06-17 22:14:58 +0100

    Fixed stupid variable names

--- simple_lsb.py
+++ simple_lsb.py
@@ -26,32 +26,32 @@ def encode_image(image,payload):
     try:
         fs = str(payload)
         f = open(fs,"rb")
-        d = list(bytearray(f.read()))
+        data = list(bytearray(f.read()))
         f.close()
     except FileNotFoundError:
         print(str(argv[0])+": \""+fs+"\": No such file or directory")
 
     w, h = im.size
-    p = list(bytearray(im.tobytes()))
+    pix = list(bytearray(im.tobytes()))
     
-    fd = []
-    ld = len(d)
-    d = [ord(i) for i in str(len(d))]+[0x00]+d
-    for a in d:
-        fd+=[a>>0b110-i*0b10 &0b11 for i in range(0b100)] # Terribly written bitshifting garbage to place the payload in the 2 LSBs of the image
+    fulldata = [] # The data once it's been split into 2 bit chunks
+    datalen=len(data) # It's nice to know the length of the data before the indicator is prepended
+    data = [ord(i) for i in str(len(data))]+[0x00]+data # Prepend length of data (stored in ascii for some reason)
+    for i in data:
+        fulldata+=[i>>0b110-j*0b10 &0b11 for j in range(0b100)] # Terribly written bitshifting garbage to place the payload in the 2 LSBs of the image
 
-    if(len(fd)>len(p)):
+    if(len(fulldata)>len(pix)):
         print(str(argv[0])+": data is too large to encode into image, get a bigger image or smaller data plz")
         return(1)
 
-    for i in range(len(fd)):
-        p[i]=(p[i]&0b11111100)+fd[i]
+    for i in range(len(fulldata)):
+        pix[i]=(pix[i]&0b11111100)+fulldata[i]
 
-    p = bytes(p)
-    nim = Image.frombytes("RGB", (w, h), p)
+    pix = bytes(pix)
+    nim = Image.frombytes("RGB", (w, h), pix)
     nim.save("new_"+ims)
 
-    print("Encoded file of length "+str(ld)+" bytes to new_"+ims)
+    print("Encoded file of length "+str(datalen)+" bytes to new_"+ims)
 
 def decode_image(image, outfile):
     try:
@@ -71,36 +71,36 @@ def decode_image(image, outfile):
         print(str(argv[0])+": \""+fs+"\": No such file or directory")
         return(1)
     
-    p = list(bytearray(im.tobytes()))
+    pix = list(bytearray(im.tobytes()))
 
-    # Get payload size
-    dl = ""
-    td = 0
-    l = 0
-    for i in range(len(p)):
-        td+=(p[i]&0b00000011)<<((3-i%4)*2)
+    # Get data size
+    datalen = ""
+    thisbyte = 0
+    length = 0
+    for i in range(len(pix)):
+        thisbyte+=(pix[i]&0b00000011)<<((3-i%4)*2) # Build current byte from LSBs
         if(3-i%4==0):
-            dl+=chr(td)
-            if(td==0x00):
+            datalen+=chr(thisbyte)
+            if(thisbyte==0x00):
                 try:
-                    l = int(dl[:-1])
+                    length = int(datalen[:-1]) # [:-1] used as length indicator is ended by null byte, this must be stripped before conversion
                 except ValueError:
                     print(str(argv[0])+": Could not find hidden file size, likely there is no hidden file in this image.")
                     return(0)
                 break
-            td = 0
+            thisbyte = 0
 
-    # Get payload
-    d = []
-    td = 0
-    for i in range(len(dl)*4,l*4+len(dl)*4):
-        td+=(p[i]&0b00000011)<<((3-i%4)*2)
+    # Get data
+    data = []
+    thisbyte = 0
+    for i in range(len(datalen)*4,length*4+len(datalen)*4):
+        thisbyte+=(pix[i]&0b00000011)<<((3-i%4)*2) # Build current byte from LSBs
         if(3-i%4==0):
-            d+=[td]
-            td = 0
-    print("Wrote file of size: "+str(l)+" bytes to: "+fs+".")
+            data+=[thisbyte]
+            thisbyte = 0
+    print("Wrote file of size: "+str(length)+" bytes to: "+fs+".")
 
-    f.write(bytes(d)) 
+    f.write(bytes(data)) 
     f.close()
     return(0)
 
